4771d4be2f33880a25a0c3e42053f9e2cb376551
buzum <buzum@mail.ru>
2023-03-20 20:23:19 +0400

    task 47 for HW 7

---
+++ task47/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+/// Создаем 2-х мерный массив
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble();
+        }
+    }
+    return result;
+}
+
+/// Печатаем  2-х мерный массив
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
